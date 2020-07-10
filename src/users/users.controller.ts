a7b951c10426be596465aec0c0fd66cfe8cad678
pedro412 <pedro.alvarez412@outlook.com>
2020-07-09 23:44:28 -0500

    Initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post, HttpCode, Param, Body } from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.interface';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(204)
+  async create(@Body() createUserDto: CreateUserDto) {
+    this.usersService.create(createUserDto);
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `user id: ${params.id}`;
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
