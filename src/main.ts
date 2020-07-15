68460aaef392556c32d476bb245b318ab23bf550
pedro412 <pedro.alvarez412@outlook.com>
2020-07-14 22:19:12 -0500

    [ADD] Cors enable

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.use(compression());
   await app.listen(3000);
